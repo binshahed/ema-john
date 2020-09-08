8e6c11d0d83c6d0e5c44d75eaa9e692e5186840b
Shahed Ahmed <mdbinshahed5@gmail.com>
2020-09-08 17:49:13 +0600

    Shoppin Cart Add

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -11,7 +11,7 @@ const [cart, setCart]=useState([])
 
 const handleAddProduct=(product)=>{
     console.log('Product add',product)
-    const newCart=[...cart, products]
+    const newCart=[...cart, product]
     setCart(newCart)
 }
 
